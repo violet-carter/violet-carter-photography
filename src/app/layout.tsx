5ae7278e506c60f52901b0907699c4f1fc8826dd
Violet Carter <violet@iMac.lan>
2025-08-21 19:49:58 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -207,6 +208,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Analytics />
         {/* Manifest for PWA */}
         <link rel="manifest" href="/manifest.json" />
         
